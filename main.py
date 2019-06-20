eab26fa77477e5d58273958ea26b75b910914667
Cindy Kim <cindykim100@gmail.com>
2019-06-19 20:57:39 -0700

    Updates on Sprites
    1) Inserted clock tick for fps (frames per second) 
    2) Created the redrawGameWindow() so that the sprite can update every time the basket catches a fruit
    - max_fruit for the number of fruits the basket holds when it's full
    - fruit_count to update the sprite every time the basket catches a fruit
    
    CONCERNS:
    1) Not sure how to code the indicator for if the basket catches the fruit...

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ pygame.display.set_caption("Blob Game")
 #bomb = pygame.image.load('__.jpg')
 #bg = pygame.image.load('__.jpg')
 #char = pygame.image.load('___.jpg')
+#basket = [LIST OF IMAGES OF BASKETS WITH FRUITS, pygame.image.load('___.jpg)]
+
+clock = pygame.time.Clock()
+fruit_count = 0
+max_fruit = 5
 
 class Player(object):
     def __init__(self, x, y, width, height):
@@ -30,17 +35,28 @@ class Fruit(object):
         while self.y < 800 + blob.height:
             self.y += self.vel
 
-blob = Player(250, 750)
+def redrawGameWindow():
+    #win.blit(bg, (0,0))
+    if (fruit_count < max_fruit):
+        win.blit(basket[fruit_count], (x,y))
+    else:
+        win.blit(basket[max_fruit], (x,y))
+    pygame.display.update()
+    
+basket = Player(250, 750)
 play = True
 while play:
+    clock.tick(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
             
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_LEFT] and blob.x > blob.vel:
+    if keys[pygame.K_LEFT] and basket.x > basket.vel:
         x -= vel
-    if keys[pygame.K_RIGHT] and blob.x < 500 - blob.width - blob.vel:
-        x += vel
-    
+    if keys[pygame.K_RIGHT] and basket.x < 500 - basket.width - basket.vel:
+        x += vel   
+    #if (basket catches fruit): increase fruit_count
+       
+redrawGameWindow()
