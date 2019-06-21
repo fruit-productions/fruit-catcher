c5081291f021b6e88810187a32106ca41f012de0
Kristel Fung <kristelfung@gmail.com>
2019-06-20 20:00:20 -0400

    Add images
    Add sprites for the fruit and basket

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ pygame.init()
 window = pygame.display.set_mode((500, 800))
 pygame.display.set_caption("Blob Game")
 
-#fruits = [LIST OF IMAGES OF FRUITS, pygame.image.load('__.jpg')]
+fruits = [pygame.image.load('strawberry.png')]
 #bomb = pygame.image.load('__.jpg')
 #bg = pygame.image.load('__.jpg')
-#char = pygame.image.load('___.jpg')
+basket = pygame.image.load('basket.png')
 
-class Player(object):
+class Basket(object):
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
@@ -30,15 +30,15 @@ class Fruit(object):
         while self.y < 800 + blob.height:
             self.y += self.vel
 
-blob = Player(250, 750)
+basket = Basket(250, 750)
 play = True
 while play:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
-            
+
     keys = pygame.key.get_pressed()
-    
+
     if keys[pygame.K_LEFT] and blob.x > blob.vel:
         x -= vel
     if keys[pygame.K_RIGHT] and blob.x < 500 - blob.width - blob.vel:
