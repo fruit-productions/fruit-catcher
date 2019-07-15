082aa448b69d3cdb8661372bb051c891b116d0b4
Cindy Kim <cindy.kim@nyu.edu>
2019-07-14 22:42:22 -0700

    Scratched Code
    - Now displays the background, basket, and strawberry
    - You can move the basket from left to right
    
    PROBLEMS:
    - Strawberry runs down the screen way too fast. There should be a clock delay for this but I'm not sure how to implement it.

--- main.py
+++ main.py
@@ -4,17 +4,17 @@
 import pygame
 pygame.init()
 
-window = pygame.display.set_mode((500, 800))
+display_width = 500
+display_height = 800
+window = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("Fruit Catcher")
 
-fruits = [pygame.image.load('strawberry.png')]
-bomb = pygame.image.load('bomb.png')
-bg = pygame.image.load('background.jpg')
 basket_img = pygame.image.load('basket.png')
+bg = pygame.image.load('background.jpg')
+fruits_img = [pygame.image.load('strawberry.png')]
+bomb_img = pygame.image.load('bomb.png')
 
 clock = pygame.time.Clock()
-fruit_count = 0
-max_fruit = 5
 
 class Basket(object):
     def __init__(self, x, y, width, height):
@@ -22,42 +22,47 @@ class Basket(object):
         self.y = y
         self.width = width
         self.height = height
-        self.vel = 5
+        self.vel = 10
+    def draw(self, window):
+        window.blit(basket_img, (self.x, self.y))
 
-class Fruit(object):
-    def __init__(self, x, y):
+class Fruits(object):
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
-        self.vel = 10
-    def appear(self, x, y):
-        # draw fruit at coordinates (x,y)
-        while self.y < 800 + basket.height:
+        self.width = width
+        self.height = height
+        self.vel = 1
+    def draw(self, window):
+        while self.y < 800:
             self.y += self.vel
-
-
-def redrawGameWindow():
-    #win.blit(bg, (0,0))
-    if (fruit_count < max_fruit):
-        win.blit(basket[fruit_count], (x,y))
-    else:
-        win.blit(basket[max_fruit], (x,y))
-    pygame.display.update()
+            window.blit(fruits_img[0], (self.x, self.y))
     
-basket = Basket(250, 750, 30, 30)
+def redrawGameWindow():
+    window.blit(bg, (0,0))
+    basket.draw(window)
+    strawberry.draw(window)
+    
+    pygame.display.update()
 
+#mainloop
+basket = Basket(display_width * 0.2, display_height * 0.7, 300, 30)
+strawberry = Fruits(display_width * 0.4, display_height * 0.1, 50, 50)
+#pygame.time.set_timer(pygame.USEREVENT+1, 600) AFK option?
 play = True
 while play:
-    clock.tick(50)
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
-
+    
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_LEFT] and basket.x > basket.vel:
+    if keys[pygame.K_LEFT] and basket.x > basket.vel - 5:
         basket.x -= basket.vel
     if keys[pygame.K_RIGHT] and basket.x < 500 - basket.width - basket.vel:
-        basket.x += basket.vel 
-    #if (basket catches fruit): increase fruit_count
-       
-redrawGameWindow()
+        basket.x += basket.vel
+    
+    redrawGameWindow()
+pygame.quit()
+
